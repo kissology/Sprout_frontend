7392fcc65dc93cd75b520e931e6b636a418d4c3b
Caitlin Ma <109817775+kissology@users.noreply.github.com>
2023-02-06 20:33:14 -0500

    started useContext

---
+++ sprout_client/src/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <nav>
+        <div className='navbar'>
+        <Link exact path to="/">
+            Home
+        </Link>
+        <br></br>
+        <Link exact path to="/browse">
+            Browse
+        </Link>
+        <br></br>
+        <Link exact path to="/care">
+            Care
+        </Link>
+        <br></br>
+        <Link exact path to="/manage">
+            Manage
+        </Link>
+        <br></br>
+        <Link exact path to="/account">
+            Account
+        </Link>
+        <br></br>
+
+
+
+        </div>
+        </nav>
+    )
+}
+
+export default Navbar;
